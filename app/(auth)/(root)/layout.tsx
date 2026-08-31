4e57db970bc659e62e9fec49874164bdc275d097
Aniruddh Sharma <aniruddhsharma669@gmail.com>
2026-08-31 21:49:44 +0530

    Update layout.tsx

--- app/(auth)/(root)/layout.tsx
+++ app/(auth)/(root)/layout.tsx
@@ -5,8 +5,8 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: {
-    template: "VibeCode - Editor ",
-    default: "Code Editor For VibeCoders - VibeCode",
+    template: "Society  ",
+    default: "Campus Circle",
   },
 };
 export default function HomeLayout({
