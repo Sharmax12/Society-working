da715af1a383aa21d8e266c9670f6e3c462bfef1
Aniruddh Sharma <aniruddhsharma669@gmail.com>
2026-09-09 10:28:43 +0530

    fix: remove unused auth helper import

--- auth.ts
+++ auth.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 
 import authConfig from "./auth.config"
 import { db } from "./lib/db";
-import { getAccountByUserId, getUserById } from "./modules/auth/actions";
+import { getUserById } from "./modules/auth/actions";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   callbacks: {
